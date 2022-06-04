9d25269ebd350c133d7397e42f9606a63a9ce2da
robert <robert@robert-ThinkPad-T450s>
2022-06-04 14:37:22 -0400

    Ignore the project files

--- union.groovy
+++ union.groovy
@@ -7,8 +7,8 @@ CSG cube = new Cube(	size,// X dimention
 CSG sphere = new Sphere(size/20*12.5).toCSG()
 // perform a union
 CSG cubePlusSphere = cube.union(sphere);
-
+CSG simpleSyntax =new Cylinder(10,40).toCSG() // a one line Cylinder
 //To union a list of CSG's together use the static unioAll
-CSG allUnion = CSG.unionAll([cubePlusSphere , cube.movex(size*1.5), sphere.movey(size*1.5)])
+CSG allUnion = CSG.unionAll([simpleSyntax,cubePlusSphere , cube.movex(size*1.5), sphere.movey(size*1.5)])
 
 return allUnion
