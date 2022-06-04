f263d8a1e881ef09e4d922f19632edd374419a28
robert <robert@robert-ThinkPad-T450s>
2022-06-04 14:53:35 -0400

    add a cylinder
    study

--- union.groovy
+++ union.groovy
@@ -1,3 +1,7 @@
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cylinder
+import eu.mihosoft.vrl.v3d.Sphere
+
 double size =40;
 CSG cube = new Cube(	size,// X dimention
 			size,// Y dimention
@@ -7,7 +11,8 @@ CSG cube = new Cube(	size,// X dimention
 CSG sphere = new Sphere(size/20*12.5).toCSG()
 // perform a union
 CSG cubePlusSphere = cube.union(sphere);
-CSG simpleSyntax =new Cylinder(10,40).toCSG() // a one line Cylinder
+CSG simpleSyntax =new Cylinder(20,40).toCSG() // a one line Cylinder
+.movez(45)
 //To union a list of CSG's together use the static unioAll
 CSG allUnion = CSG.unionAll([simpleSyntax,cubePlusSphere , cube.movex(size*1.5), sphere.movey(size*1.5)])
 
